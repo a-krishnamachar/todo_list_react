af82dcc71609397d6f711feb064fb7df012d9743
Aditya Krishnamachar <adityakrishnamachar@gmail.com>
2019-12-03 23:11:44 -0600

    Updates

---
+++ precache-manifest.f51537b2cec38e527ca5f692d1e3109c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "44add2c6fdf5d916b145",
+    "url": "/09-todo-react-a-krishnamachar/static/css/main.9ee9e4f1.chunk.css"
+  },
+  {
+    "revision": "44add2c6fdf5d916b145",
+    "url": "/09-todo-react-a-krishnamachar/static/js/main.44add2c6.chunk.js"
+  },
+  {
+    "revision": "939cbde5f9c76620fa49",
+    "url": "/09-todo-react-a-krishnamachar/static/js/1.939cbde5.chunk.js"
+  },
+  {
+    "revision": "7abbeb4c6fdc29e58347",
+    "url": "/09-todo-react-a-krishnamachar/static/js/runtime~main.7abbeb4c.js"
+  },
+  {
+    "revision": "bc2ff6af40dd72776865a52cf646f309",
+    "url": "/09-todo-react-a-krishnamachar/index.html"
+  }
+];
